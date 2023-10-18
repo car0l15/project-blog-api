e6519d1d3b73b1e7d4ab7ad6e5dacc1a2690d99d
car0l15 <carol.nepomuceno95@gmail.com>
2023-10-18 17:19:48 -0300

    feat: iniciando configurações do servidor

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 8000;
+
+app.get('/', (_req, res) => {
+    res.status(200).send('express + TypeScript UP');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
